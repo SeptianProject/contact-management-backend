040e9d132dde127966e9752f951dc9062ba2b5ac
SeptianProject <septianuwoe75@gmail.com>
2025-12-25 20:59:58 +0700

    feat: add dotenv support and update database configuration

--- src/application/database.js
+++ src/application/database.js
@@ -1,7 +1,8 @@
-import {PrismaClient} from "@prisma/client";
-import {logger} from "./logging.js";
+import { PrismaClient } from "@prisma/client";
+import { logger } from "./logging.js";
 
 export const prismaClient = new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
     log: [
         {
             emit: 'event',
